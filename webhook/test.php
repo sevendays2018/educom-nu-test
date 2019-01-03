e1ec406d8fecf7aeef85868ab8498b56aef2ce8f
sevendays2018 <39212577+sevendays2018@users.noreply.github.com>
2019-01-03 14:11:08 +0700

    Update test.php

--- webhook/test.php
+++ webhook/test.php
@@ -33,7 +33,8 @@ $result = curl_exec($ch);
 
 curl_close($ch);
         
-$finale = json_decode($result, true);       
+$finale = json_decode($result, true);  
+        $data = json_encode($finale);
  
 
         
@@ -54,7 +55,7 @@ $finale = json_decode($result, true);
 
           "type" => "text",
 
-          "text" => "เรียกบ่อยๆ ระวังไว้เถอะ ".$finale["displayName"]." แร้วจะหาว่าไม่เตือน!"
+          "text" => "เรียกบ่อยๆ ระวังไว้เถอะ ".$data." แร้วจะหาว่าไม่เตือน!"
 
 ))
 
